2ce0129a2399dabf9a573b47463ff4fb32d89977
Akhil <gamerunknownyt0@gmail.com>
2024-01-12 17:11:00 +0545

    Interface Final(almost)

--- public/js/fee.js
+++ public/js/fee.js
@@ -1,12 +1,19 @@
 
   function showViewStructureButton() {
+    var semesterfee= document.getElementById("table");
     var feeType = document.getElementById("fee-type");
     var secondDropdownContainer = document.getElementById("secondDropdownContainer");
+    var pay= document.getElementById("pay");
 
     if (feeType.value === "semester") {
       secondDropdownContainer.style.display = "block";
+      semesterfee.style.display = "block";
+      pay.style.display="block";
     } else {
       secondDropdownContainer.style.display = "none";
+      semesterfee.style.display = "none";
+      pay.style.display="none";
+
     }
     var feeTypeSelect = document.getElementById("fee-type");
     var viewStructureButton = document.getElementById("view-structure-button");
